0c907e9543f173c82680cb58ca1246ee08b0a994
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-02-22 16:49:20 +0800

    Add files via upload

--- graduation photo/index3.js
+++ graduation photo/index3.js
@@ -1,10 +1,15 @@
 let currentPage = 0;
 const pages = document.querySelectorAll('.page');
+const rightPages = document.querySelectorAll(".right-page");
+const leftPages = document.querySelector(".left-page");
 
 function showPage(index){
     pages.forEach((page, i) => {
         page.style.zIndex = pages.length - i;
-        page.classList.toggle("hidden". i < index);
+        page.classList.toggle("hidden", i < index);
+    });
+    leftPages.forEach((page, i) => {
+        page.classList.toggle("up", i < index);
     });
 }
 
@@ -17,5 +22,6 @@ function flipPage(direction){
         currentPage--;
         pages[currentPage].classList.remove('flipped');
     }
+    
 }
-showPage(0);
+showPage(0 + i++);
