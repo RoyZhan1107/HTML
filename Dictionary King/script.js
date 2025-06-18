8280328d3ba4787678558c336b848ab8bd565d58
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-06-18 21:18:16 +0800

    Add files via upload

---
+++ Dictionary King/script.js
@@ -0,0 +1,22 @@
+// rules
+function toggle(id){
+    const menu = document.getElementById(id);
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+// generate
+let words = [];
+fetch('vocabulary.txt')
+    .then(res => res.text())
+    .then(text => {
+        words = text.split('\n').map(w => w.trim()).filter(Boolean);
+    });
+
+function generate(){
+    if(words.length === 0){
+        document.getElementById("word").textContent = 'No Loading Vocabulary';
+        return;
+    }
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    document.getElementById("word").textContent = randomWord;
+}
