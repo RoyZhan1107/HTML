e1bcb05e50ec181470b8903f986c380293ba814c
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-07-14 16:00:52 +0800

    Add files via upload

--- Music-Player/lyrics.js
+++ Music-Player/lyrics.js
@@ -58,7 +58,7 @@ player.addEventListener("timeupdate", () => {
 
     const currentLine = document.getElementById(`line-${currentLineIndex}`);
     if (currentLine) {
-      currentLine.scrollIntoView({ behavior: "smooth", block: "start" });
+      currentLine.scrollIntoView({ top: lineTop, behavior: "smooth" });
     }
   }
 });
