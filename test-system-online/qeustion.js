c35d0ef26a5bcd21ea3004df632fa11cd6674752
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-07-22 22:07:05 +0800

    Add files via upload

--- test-system-online/qeustion.js
+++ test-system-online/qeustion.js
@@ -19,16 +19,24 @@ function loadQuestion(category){
 // 載入題目
 function displayQuestion(){
     const q = questions[currentIndex];
+    const imgElement = document.getElementById("que-img");
     document.getElementById("question").textContent = q.question;
 
     document.querySelector("label[for='option1']").textContent = q.options[0];
     document.querySelector("label[for='option2']").textContent = q.options[1];
     document.querySelector("label[for='option3']").textContent = q.options[2];
     document.querySelector("label[for='option4']").textContent = q.options[3];
-
+    
     ['option1', 'option2', 'option3', 'option4'].forEach(id => {
         document.getElementById(id).checked = false;
     });
+    
+    if(q.image){
+        imgElement.src = q.image;
+        imgElement.style.display = "block";
+    }else{
+        imgElement.style.display = "none";
+    }
 }
 
 function Next(){
