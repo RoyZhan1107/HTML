fb83ea033626a072da4a0aab13fc623308e76f5c
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-07-23 14:04:17 +0800

    Add files via upload

--- test-system-online/qeustion.js
+++ test-system-online/qeustion.js
@@ -2,40 +2,30 @@
 let questions = [];
 let currentIndex = 0;
 // 根據職類名稱正確載入
-function loadQuestion(category){
-    fetch(`question/${category}.json`)
-        .then(res => {
-            if(!res.ok) throw new Error('載入失敗');
-            return res.json();
-        })
-        .then(data => {
-            questions = data;
-            currentIndex = 0;
-            displayQuestion();
-        })
-        .catch(err => console.error('讀到 JSON 但顯示失敗:', err));
-}
+fetch("question/${category}.json")
+.then(res => res.json())
+.then(data => {
+    questions = data;
+    renderQuestion(currentIndex);
+})
+.catch(err => console.error('讀到 JSON 但顯示失敗:', err));
 
 // 載入題目
-function displayQuestion(){
-    const q = questions[currentIndex];
-    const imgElement = document.getElementById("que-img");
+function renderQuestion(index){
+    const q = questions[index];
     document.getElementById("question").textContent = q.question;
 
-    document.querySelector("label[for='option1']").textContent = q.options[0];
-    document.querySelector("label[for='option2']").textContent = q.options[1];
-    document.querySelector("label[for='option3']").textContent = q.options[2];
-    document.querySelector("label[for='option4']").textContent = q.options[3];
-    
-    ['option1', 'option2', 'option3', 'option4'].forEach(id => {
-        document.getElementById(id).checked = false;
-    });
-    
+    // 顯示圖片 (如果有)
+    const imgEl = document.getElementById("que-img");
     if(q.image){
-        imgElement.src = q.image;
-        imgElement.style.display = "block";
+        imgEl.src = q.image;
+        imgEl.style.display = "block";
     }else{
-        imgElement.style.display = "none";
+        imgEl.style.display = "none";
+    }
+    // 顯示選項
+    for(let i = 0; i < 4; i++){
+        document.getElementById(`label${i + 1}`).textContent = q.options[i];
     }
 }
 
